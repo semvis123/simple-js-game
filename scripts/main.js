45ffa22b6c8fecc15af9dbc39a62b515acfc8d7b
semvis123 <semvisscher10@gmail.com>
2019-02-04 14:14:05 +0000

    update v0.0.3

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,6 @@
-let blocks=[[],[],[]];
+let blocks=[[0,0,0],
+            [0,0,0],
+            [0,0,0]];
 class Game {
     constructor(canvas, width, height) {
         canvas.width = width;
@@ -7,17 +9,26 @@ class Game {
         this._height = height;
         this._ctx = canvas.getContext('2d'); // store context to draw something
     }
-
-    play() {
+    _createBlocks() {
+        blocks.forEach(function (row,i){
+            row.forEach(function (column,j){
+                blocks[i][j] = new Block(i,j);
+                console.log(blocks[i][j]);
+            });
+        });
+    }
+    _play() {
         this._clear(); // clear the whole canvas to draw something new
         this._drawBorder(); // draw a game area border
         this._drawBlocks();
-        requestAnimationFrame(this.play.bind(this)); // run play again ~60 times per sec    
+        requestAnimationFrame(this._play.bind(this)); // run play again ~60 times per sec    
     }
-    _drawBlocks(){
-        blocks.forEach(function (row){
-            row.forEach(function (block));
-        })
+    _drawBlocks() {
+        blocks.forEach(function (row,i){
+            row.forEach(function (column,j){
+                blocks[i][j]._draw();
+            });
+        });
     }
     _drawBorder() {
         this._ctx.beginPath();
@@ -30,10 +41,26 @@ class Game {
     }
 }
 class Block{
-    constructor(){
-        this.isSet=false;
+    constructor(row,column) {
+        this._game = game;
+        this._row = row;
+        this._column = column;
+        this._blockWidth = game._width/3;
+        this._blockHeight = game._height/3;
+        this._isSet = false;
+        this._state = 'x';
+        this._x = this._column*this._blockWidth;
+        this._y = this._row*this._blockHeight;
     }
-    
+    _draw() {
+        this._game._ctx.beginPath();
+        this._game._ctx.rect(this._x, this._y, this._blockWidth, this._blockHeight);
+        this._game._ctx.stroke();
+        this._game._ctx.font = "30px Ariel";
+        this._game._ctx.fillStyle = "red";
+        this._game._ctx.fillText(this._state, this._x+(this._blockWidth / 2)-2, this._y+(this._blockHeight / 2)-2);
+    }
 }
 var game = new Game(document.getElementsByTagName('canvas')[0], 300, 300); // create an instance of the game
-game.play(); // start it
+game._createBlocks();
+game._play(); // start it
